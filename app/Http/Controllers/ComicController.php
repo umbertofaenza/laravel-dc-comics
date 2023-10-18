8f66997f05c90e197673ac0fba3c4603ae72bd5b
umbertofaenza <umberto.faenza@gmail.com>
2023-10-18 17:20:42 +0200

    populate store method

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -34,11 +34,17 @@ class ComicController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     ** @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+
+        $comic = new Comic();
+        $comic->fill($data);
+        $comic->save();
+
+        return redirect()->route('comics.index');
     }
 
     /**
