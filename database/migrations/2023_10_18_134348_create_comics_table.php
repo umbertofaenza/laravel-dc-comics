a341184b1e8993c284e31e7aed5d7de0f08ad849
umbertofaenza <umberto.faenza@gmail.com>
2023-10-18 16:03:07 +0200

    add values for migration

--- database/migrations/2023_10_18_134348_create_comics_table.php
+++ database/migrations/2023_10_18_134348_create_comics_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -15,6 +14,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
