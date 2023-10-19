aff0a94a10c3f4b4b78852fbbf32281dcc468fa6
umbertofaenza <umberto.faenza@gmail.com>
2023-10-19 16:21:09 +0200

    add modal to double-check delete

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,12 +6,13 @@
 
         <a href="{{ route('comics.edit', $comic) }}" class="btn btn-warning mb-3">Edit comic</a>
 
-        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-            @csrf
-            @method('DELETE')
+        <button type="button" class="btn btn-danger mb-3" data-bs-toggle="modal"
+            data-bs-target="#deleteModal-{{ $comic->id }}">
+            Delete comic
+        </button>
 
-            <button class="btn btn-danger mb-3">Delete comic</button>
-        </form>
+
+
 
         <div class="row">
             <div class="col-12">
@@ -27,5 +28,31 @@
                 </div>
             </div>
         </div>
+
+        {{-- deleteModal --}}
+        <div class="modal fade" id="deleteModal-{{ $comic->id }}" tabindex="-1" aria-labelledby="deleteModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="deleteModalLabel">DELETING COMIC</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure you want to delete this comic?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-warning" data-bs-dismiss="modal">Cancel</button>
+                        <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+
+                            <button class="btn btn-danger">Delete comic</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </section>
 @endsection
